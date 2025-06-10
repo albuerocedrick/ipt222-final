c9b0480b0118fdca405e0ea521fcee8ff86b3d33
cedd <albuerocedrick@gmail.com>
2025-06-11 00:06:00 +0800

    updated

--- frontend/src/views/Main/Settings.vue
+++ frontend/src/views/Main/Settings.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>
-        
+    <div class="flex flex-row w-full h-full gap-8">
+        <setting-menu />
+        <setting-content />
     </div>
 </template>
+
+<script>
+import SettingMenu from './SettingsPage/SettingMenu.vue';
+import SettingContent from './SettingsPage/SettingContent.vue';
+
+export default {
+    components: {
+        SettingMenu,
+        SettingContent
+    }
+}
+</script>
---
+++ frontend/src/views/Main/SettingsPage/SettingContent.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="w-3/5 box bg-base-100">
+
+    </div>
+</template>
